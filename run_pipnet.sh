e25c92482e52ab273c83a0ed4b5cd57e7d812893
Harish Babu Manogaran <harishbabu@tinkercliffs1.cm.cluster>
2024-05-13 23:25:31 -0400

    minor change

--- run_pipnet.sh
+++ run_pipnet.sh
@@ -29,12 +29,12 @@
 
 # DO THIS AFTER TRAINING WHEELS -|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
 # set finetune back to 5, epochs_pretrain=30, epochs=60, freeze_epochs=10
-python main.py --log_dir './runs/212-DinoV2_PruningBF=1.1NaiveHPIPNetMaskL1=0.5MaskTrainExtra=05epsEps=60Cl=2.0TanhDesc=0.05MinCont=0.1_CUB-190-imgnet-224_WeightedCE_with-equalize-aug_img=224_nprotos=10pc' \
+python main.py --log_dir './runs/219-178like_nprotos=30pc-cnext26_PruningBF=1.1NaiveHPIPNetMaskL1=0.5MaskTrainExtra=05epsEps=60Cl=2.0TanhDesc=0.05MinCont=0.1_CUB-190-imgnet-224_WeightedCE_with-equalize-aug_img=224' \
                --training_wheels "n" \
                --copy_files "y" \
                --wandb "y" \
                --dataset CUB-190-imgnet-224 \
-               --net dinov2_vits14_reg \
+               --net convnext_tiny_26 \
                --batch_size 256 \
                --batch_size_pretrain 256 \
                --epochs 75 \
@@ -52,14 +52,14 @@ python main.py --log_dir './runs/212-DinoV2_PruningBF=1.1NaiveHPIPNetMaskL1=0.5M
                --state_dict_dir_net '' \
                --dir_for_saving_images 'Visualization_results' \
                --seed 1 \
-               --gpu_ids '0,1' \
+               --gpu_ids '0,1,2' \
                --num_workers 8 \
                --phylo_config ./configs/cub190_phylogeny.yaml \
                --experiment_note "" \
                --kernel_orth "y" \
                --num_features 0 \
                --num_protos_per_descendant 0 \
-               --num_protos_per_child 10 \
+               --num_protos_per_child 30 \
                --align "n" \
                --uni "n" \
                --align_pf "y" \
